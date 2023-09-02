97deccaf4564e36ba97e2da51d92f6e8c673e563
Parul Gupta <112709768+parulgupta27@users.noreply.github.com>
2023-09-02 16:01:54 +0530

    Delete src/Env directory

--- src/Env/prod.env.ts
+++
@@ -1,6 +0,0 @@
-import {Environment} from "./env.env";
- 
- var Production:Environment={
-    db_url:"mongodb+srv://theparul_gupta:Parul2711@cluster0.npeikkx.mongodb.net/?retryWrites=true&w=majority",
-}
-export default Production;
